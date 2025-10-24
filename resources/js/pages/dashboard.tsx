a6f8a11d9b294530bf9e9f01403e8fda36c3c40f
Roberth <roberthrs109@gmail.com>
2025-10-23 20:37:50 -0500

    Ultimos detalles

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -284,7 +284,7 @@ export default function Dashboard({
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title={`Dashboard ${yearCurrent} - Biblioteca Digital`} />
+            <Head title={`Dashboard ${yearCurrent}`} />
 
             <div className="min-h-screen bg-gray-50 dark:bg-gray-900 py-6 px-4 sm:px-6 lg:px-8">
                 <div className="max-w-7xl mx-auto">
